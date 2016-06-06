36fdc4d02c8cf4082a2b196f49563d74839d0fc5
Grace <gracem@thoughtworks.com>
2016-06-06 15:44:43 +0200

    Grace/Hlulani: added another tests covering scenarios from 10 to 19

--- src/main/java/com/thoughtworks/romanNumeral/tdd/RomansNumbers.java
+++ src/main/java/com/thoughtworks/romanNumeral/tdd/RomansNumbers.java
@@ -4,26 +4,39 @@ public class RomansNumbers {
     public String getRomanNumbers(int number) throws RomansNumbersException {
 
         String romans = "";
+        String[] romansSymbols = {"I", "IV", "V", "IX", "X"};
+        int[] arabicNumbers = {1, 4, 5, 9, 10};
 
         if (number == 0) {
             throw new RomansNumbersException("Zero number does not exist in Romans Number");
+        }
 
-        } else if (number <= 8 && number >= 5) {
-            romans = "V";
+        while(number >= arabicNumbers[4]){
 
-            for (int i = 5; i < number; i++){
+            romans = romansSymbols[4];
+            number -= arabicNumbers[4];
+        }
+
+        if (number <= 8 && number >= 5) {
+            romans += "V";
+
+            for (int i = 5; i < number; i++) {
                 romans += "I";
             }
 
-        } else if(number <= 3){
-            romans = "I";
+        } else if (number <= 3 && number > 0) {
+            romans += "I";
 
-            for(int i = 1; i < number; i++){
+            for (int i = 1; i < number; i++) {
                 romans += "I";
             }
 
-        } else if (number == 4){
-            romans = "IV";
+        } else if (number == 4) {
+            romans += "IV";
+
+        } else if (number == 9){
+            romans += "IX";
+
         }
 
         return romans;
--- src/test/java/com/thoughtworks/romanNumeral/tdd/RomansNumbersTest.java
+++ src/test/java/com/thoughtworks/romanNumeral/tdd/RomansNumbersTest.java
@@ -1,11 +1,19 @@
 package com.thoughtworks.romanNumeral.tdd;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class RomansNumbersTest {
+
+    private RomansNumbers romansNumbers;
+
+    @Before
+    public void setUp() throws Exception {
+        romansNumbers = new RomansNumbers();
+    }
 
     @Rule
     public ExpectedException error = ExpectedException.none();
@@ -14,54 +22,87 @@ public class RomansNumbersTest {
     public void testZeroThrowException() throws RomansNumbersException {
         error.expect(RomansNumbersException.class);
         error.expectMessage("Zero number does not exist in Romans Number");
-        RomansNumbers romansNumbers = new RomansNumbers();
         romansNumbers.getRomanNumbers(0);
     }
 
     @Test
     public void testOneReturnI() throws Exception {
-        RomansNumbers romansNumbers = new RomansNumbers();
         Assert.assertEquals("I", romansNumbers.getRomanNumbers(1));
     }
 
     @Test
     public void testTwoReturnII() throws Exception {
-        RomansNumbers romansNumbers = new RomansNumbers();
         Assert.assertEquals("II", romansNumbers.getRomanNumbers(2));
     }
 
     @Test
     public void testThreeReturnIII() throws Exception {
-        RomansNumbers romansNumbers = new RomansNumbers();
         Assert.assertEquals("III", romansNumbers.getRomanNumbers(3));
     }
 
     @Test
     public void testFourReturnIV() throws Exception {
 
-        RomansNumbers romansNumbers = new RomansNumbers();
         Assert.assertEquals("IV", romansNumbers.getRomanNumbers(4));
     }
 
     @Test
     public void testFiveReturnV() throws Exception {
-        RomansNumbers romansNumbers = new RomansNumbers();
         Assert.assertEquals("V", romansNumbers.getRomanNumbers(5));
     }
 
     @Test
     public void testSixReturnVI() throws Exception {
-        RomansNumbers romansNumbers = new RomansNumbers();
         Assert.assertEquals("VI", romansNumbers.getRomanNumbers(6));
     }
 
     @Test
     public void testSevenReturnVII() throws Exception {
-        RomansNumbers romansNumbers = new RomansNumbers();
         Assert.assertEquals("VII", romansNumbers.getRomanNumbers(7));
     }
 
     @Test
     public void testEightReturnVIII() throws Exception {
+        Assert.assertEquals("VIII", romansNumbers.getRomanNumbers(8));
+    }
+
+    @Test
+    public void testNineReturnIX() throws Exception {
+        Assert.assertEquals("IX", romansNumbers.getRomanNumbers(9));
+    }
+
+    @Test
+    public void testTenReturnX() throws Exception {
+        Assert.assertEquals("X", romansNumbers.getRomanNumbers(10));
+    }
+
+    @Test
+    public void testElevenReturnXI() throws Exception {
+        Assert.assertEquals("XI", romansNumbers.getRomanNumbers(11));
+    }
+
+    @Test
+    public void testTwelveReturnXII() throws Exception {
+        Assert.assertEquals("XII", romansNumbers.getRomanNumbers(12));
+    }
+
+    @Test
+    public void testThirteenReturnXIII() throws Exception {
+        Assert.assertEquals("XIII", romansNumbers.getRomanNumbers(13));
+    }
+
+    @Test
+    public void testFourteenReturnXIV() throws Exception {
+        Assert.assertEquals("XIV", romansNumbers.getRomanNumbers(14));
+    }
+
+    @Test
+    public void testFifteenReturnXV() throws Exception {
+        Assert.assertEquals("XV",romansNumbers.getRomanNumbers(15));
+    }
+
+    @Test
+    public void testNineteenReturnXIX() throws Exception {
+        Assert.assertEquals("XIX", romansNumbers.getRomanNumbers(19));
     }
 }
